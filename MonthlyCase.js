be7a67f6c617259c0c1ec4a405c90c7729495b20
sudiptobilu <75963902+sudiptobilu@users.noreply.github.com>
2022-07-23 12:27:00 -0400

    Update MonthlyCase.js

--- MonthlyCase.js
+++ MonthlyCase.js
@@ -1,6 +1,8 @@
 var MonthlyCase={
     display: async function () {
 d3.select("#MonthlyCase").selectAll("#stateDiv").remove();
+	    d3.select("#MonthlyCase").selectAll("text").remove();
+      d3.select("#MonthlyCase").selectAll("svg").remove();
 
   var margin = {top: 10, right: 30, bottom: 50, left: 60},
     width = 1400 - margin.left - margin.right,
@@ -68,80 +70,108 @@ d3.select("#stateButton").on("change", function(d) {
   update(selectedOption)
 });
 
-function update(selectedState) {
-            var csdata = statelist.filter(statelist => statelist.state == selectedState);
-            var max = d3.max(csdata, function(d) { return parseInt(d.incidents); });
+function update(selectedState) {	
+	      d3.select("#MonthlyCase").selectAll("text").remove();
+      d3.select("#MonthlyCase").selectAll("svg").remove();
 
-            var x = d3.scaleBand()
-            .range([ 0, width ])
-            .domain(csdata.map(function(d) { return d.date; }))
-            .padding(0.2);
-          
-            d3.select("#x")
-            .attr("transform", "translate(0," + height + ")")
-            .transition()
-            .duration(1000)
-            .call(d3.axisBottom(x))
-            .selectAll("text")
-              .attr("transform", "translate(-10,0)rotate(-45)")
-              .style("text-anchor", "end")
-             
-            
-          
-          // Y axis
-          console.log(max);
-          var y = d3.scaleLinear()
-            //.transition()
-            //.duration(1000)
-            .domain([0, max])
-            .range([ height, 0]);
-          d3.select("#y")
-          .transition()
-          .duration(1000)
-            .call(d3.axisLeft(y));
-          
-            svg.selectAll("#bar").remove();
-            svg.selectAll("bar")
-            .data(csdata)
-            //.transition()
-            //.duration(1000)
-            .enter()
-            .append("rect")
-            .attr("id","bar")
-              .attr("x", function(d) { return x(d.date); })
-              .attr("y", function(d) { return y(parseInt(d.incidents)); })
-              .attr("width", x.bandwidth())
-              .attr("height", function(d) {return height - y(parseInt(d.incidents)); })
-              .attr("fill", "magenta")
-              .on("mouseover", function(d) {
-                console.log(d.date);
-                if(d3.select(this).style("opacity") != 0){
-                  d3.select(this).transition()        
-                      .duration(200)      
-                      .style("opacity", .85); 
-              }
-              var html  = "<span style = 'font-size:15px;color:black'><b>" + d.date + "</b></span></br>" +
-              "<span style='font-size:12px;color:black'><b> Cases: </b>" + d.incidents + "</span></br>" +
-              "<span style='font-size:12px;color:black'><b> Deaths: </b>" + d.killed + "</span>";
-              div.transition()
-              .duration(200)
-              .style("opacity", 1);
-              div.html(html)
-              .style("left", (d3.event.pageX + 10) + "px")
-              .style("top", (d3.event.pageY +10 ) + "px")
-              .style("width",130)
-              .style("height",80)
-              .style("background",function(){ return("lightblue");})
-          
-              })
-              .on("mouseout", function(d) {
-                  div.transition()
-                  .duration(300)
-                  .style("opacity", 0);
-                  d3.select(this).transition()        
-                          .duration(200)      
-                          .style("opacity", 1); 
-              })
+            var svg = d3.select("#MonthlyCase")
+          .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform",
+                  "translate(" + margin.left + "," + margin.top + ")"); 
+  
+  var val = selectedState;
+ var csdata = statelist.filter(statelist => statelist.state == val);
+  var max = d3.max(csdata, function(d) { return parseInt(d.incidents); });
+  //X axis
+  var x = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(csdata.map(function(d) { return d.date; }))
+  .padding(0.2);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .attr("id","x")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+    svg.append("text")
+    .attr("class", "x label")
+    //.attr("text-anchor", "end")
+    .attr("x", width )
+    .attr("y", height - 6)
+    .text("Month");
+
+// Y axis
+console.log(max);
+var y = d3.scaleLinear()
+  .domain([0, max])
+  .range([ height, 0]);
+svg.append("g")
+  .attr("id","y")
+  .call(d3.axisLeft(y));
+svg.append("text")
+  .attr("class", "y label")
+  .attr("text-anchor", "end")
+  .attr("x",18)
+  .attr("y",-60)
+  .attr("dy", ".75em")
+  .attr("transform", "rotate(-90)")
+  .text("Number of Cases");
+
+  var div = d3.select("body").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
+
+   svg.selectAll("bar")
+  .data(csdata)
+  .enter()
+  .append("rect")
+    .attr("id","bar")
+    .attr("x", function(d) { return x(d.date); })
+    .attr("y", function(d) { return y(parseInt(d.incidents)); })
+    .attr("width", x.bandwidth())
+    .attr("height", function(d) {return height - y(parseInt(d.incidents)); })
+    .attr("fill", "magenta")
+    .on("mouseover", function(d) {
+      console.log(d.date);
+      if(d3.select(this).style("opacity") != 0){
+        d3.select(this).transition()        
+            .duration(200)      
+            .style("opacity", .85); 
+    }
+    var html  = "<span style = 'font-size:15px;color:black'><b>" + d.date + "</b></span></br>" +
+    "<span style='font-size:12px;color:black'><b> Cases: </b>" + d.incidents + "</span></br>" +
+    "<span style='font-size:12px;color:black'><b> Deaths: </b>" + d.killed + "</span>";
+    div.transition()
+    .duration(200)
+    .style("opacity", 1);
+    div.html(html)
+    .style("left", (d3.event.pageX + 10) + "px")
+    .style("top", (d3.event.pageY +10 ) + "px")
+    .style("width",130)
+    .style("height",80)
+    .style("background",function(){ return("lightblue");})
+
+    })
+    
+    .on("mouseout", function(d) {
+        div.transition()
+        .duration(300)
+        .style("opacity", 0);
+        d3.select(this).transition()        
+                .duration(200)      
+                .style("opacity", 1); 
+    })
+
+    var rect = svg.append('rect').attr("x", 420).transition().duration(1000).attr("y", 0).attr("width", 300)
+    .attr("height", 30).attr("fill","lightblue").attr("stroke","black").text("As of July 2022")
+
+d3.selectAll("svg").append("g").append("text").transition().duration(1000).attr("id","annotation").attr("x", 480).attr("y", 25)
+	.text("Monthly case statistics for the selected State").attr("font-size", "14px").attr("fill", "black").attr("font-weight","bold")
 
           }
 
